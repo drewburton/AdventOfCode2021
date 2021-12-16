d0df1ed345670242d87601fb75e86fad811b8e63
Drew Burton <drew.burton.gc@gmail.com>
2021-12-16 15:28:31 -0500

    finished day 8

--- day08/Main.java
+++ day08/Main.java
@@ -3,18 +3,25 @@ package day08;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
-			ArrayList<String> list = parse();
-			int count = 0;
-			for (String s : list) {
-				int num = determineNum(s);
-				if (num != -1) count++;
+			ArrayList<String> segs = parse();
+			int sum = 0;
+			for (String s : segs) {
+				HashMap<Integer, Set<Character>> signals = determineSignal(s);
+				sum += determineOutput(s, signals);
 			}
-			System.out.println(count);
+			System.out.println(sum);
 		} catch (FileNotFoundException e) { System.out.println("unable to open file"); }
 	}
 
@@ -22,25 +29,84 @@ public class Main {
 		//File file = new File("day08/test.txt");
 		File file = new File("day08/input.txt");
 		try (Scanner scanner = new Scanner(file)) {
-			ArrayList<String> list = new ArrayList<>();
+			ArrayList<String> segs = new ArrayList<>();
 			while (scanner.hasNextLine()) {
-				String[] split = scanner.nextLine().split("\\|");
-				String[] signal = split[0].trim().split(" ");
-				String[] output = split[1].trim().split(" ");
-				//for (String s : signal) list.add(s);
-				for (String s : output) list.add(s);
+				segs.add(scanner.nextLine());
 			}
-			return list;
+			return segs;
 		}
 	}
 
-	private static int determineNum(String s) {
-		switch(s.length()) {
-			case 2: return 1;
-			case 3: return 7;
-			case 4: return 4;
-			case 7: return 8;
-			default: return -1;
+	private static HashMap<Integer, Set<Character>> determineSignal(String seg) {
+		String[] split = seg.split("\\|");
+		String[] signal = split[0].trim().split(" ");
+
+		HashMap<Integer, Set<Character>> map = new HashMap<>();
+
+		// uniques
+		map.put(1, set(getSegmentsOfLength(signal, 2).get(0)));
+		map.put(4, set(getSegmentsOfLength(signal, 4).get(0)));
+		map.put(7, set(getSegmentsOfLength(signal, 3).get(0)));	
+
+		List<String> fives = getSegmentsOfLength(signal, 5);
+		for (String lenFive : fives) {
+			Set<Character> set1 = set(lenFive);
+			Set<Character> set2 = set(lenFive);
+			set1.retainAll(map.get(1));
+			set2.retainAll(map.get(4));
+
+			if (set1.size() == 2) {
+				map.put(3, set(lenFive));
+			} else if (set2.size() == 3) {
+				map.put(5, set(lenFive));
+			} else {
+				map.put(2, set(lenFive));
+			}
 		}
+
+		List<String> sixes = getSegmentsOfLength(signal, 6);
+		for (String lenSix : sixes) {
+			Set<Character> set1 = set(lenSix);
+			Set<Character> set2 = set(lenSix);
+			set1.retainAll(map.get(4));
+			set2.retainAll(map.get(7));
+
+			if (set1.size() == 4) {
+				map.put(9, set(lenSix));
+			} else if (set2.size() == 3) {
+				map.put(0, set(lenSix));
+			} else {
+				map.put(6, set(lenSix));
+			}
+		}
+
+		map.put(8, set("abcdefg"));
+		return map;
+	}
+
+	private static int determineOutput(String seg, HashMap<Integer, Set<Character>> signals) {
+		String[] output = seg.split("\\|")[1].trim().split(" ");
+		String result = "";
+		for (String s : output) {
+			Set<Character> set = set(s);
+			for (Entry e : signals.entrySet()) {
+				if (e.getValue().equals(set)) {
+					result += e.getKey() + "";
+				}
+			}
+		}
+		return Integer.parseInt(result);
+	}
+
+	private static List<String> getSegmentsOfLength(String[] signal, int length) {
+		return Stream.of(signal).filter(s -> s.length() == length).collect(Collectors.toList());
+	}
+
+	private static Set<Character> set(String s) {
+		Set<Character> set = new HashSet<>();
+		for (int i = 0; i < s.length(); i++) {
+			set.add(s.charAt(i));
+		}
+		return set;
 	}
 }
